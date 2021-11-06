cc312068a2797bc4e41591305634ef73eaf0e43e
Juan Badano <jb@target-solutions.net>
2021-11-06 20:36:15 +0200

    asd

--- src/dataListItem.js
+++ src/dataListItem.js
@@ -1,9 +1,8 @@
 import React from 'react'
-
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
-export const DataListItem = ({ label, primary, secondary }) => (
+const DataListItem = ({ label, primary, secondary }) => (
   <Box display={{ xs: 'block', md: 'flex' }} alignItems="center" width="100%">
     <Box p={1} width={{ xs: '100%', md: '40%' }}>
       <Typography
